26b3da0b3df3ce1aa6a3b0a1a57032d1e5f1f05e
anandkumarpatel1 <anandkumar7368970125@gmail.com>
2024-09-02 17:26:49 +0530

    fix post algo

--- app/(tabs)/addPosts.jsx
+++ app/(tabs)/addPosts.jsx
@@ -13,13 +13,25 @@ import {
 import * as ImagePicker from "expo-image-picker";
 import { Video } from "expo-av";
 import { useRouter } from "expo-router";
+import AsyncStorage from "@react-native-async-storage/async-storage";
+import Alert from "../../components/Alert";
+import { ref, getDownloadURL, uploadBytesResumable } from "firebase/storage";
+import { storage } from "../../firebase";
+import { useRecoilValue } from "recoil";
+import userAtom from "../../atoms/userAtom";
+import axios from "axios";
 
 const addPosts = () => {
   const [postContent, setPostContent] = useState();
   const [contentType, setContentType] = useState();
+  const [loading, setLoading] = useState(false);
+  const [alert, setAlert] = useState(false);
+  const [progress, setProgress] = useState(0);
+  const [message, setMessage] = useState();
+  const [desc, setDesc] = useState("");
   const video = useRef(null);
   const router = useRouter();
-
+  const user = useRecoilValue(userAtom)
   const windowHeight = Dimensions.get("window").height;
   const windowWidth = Dimensions.get("window").width;
 
@@ -35,7 +47,7 @@ const addPosts = () => {
     let result = await ImagePicker.launchImageLibraryAsync({
       mediaTypes: ImagePicker.MediaTypeOptions.All,
       allowsEditing: true,
-      quality: 1,
+      quality: 0.4,
       aspect: [4, 3],
     });
 
@@ -50,7 +62,88 @@ const addPosts = () => {
     }
   };
 
-  const postHandler = () => {};
+  const postHandler = async () => {
+    try {
+      setLoading(true);
+      if (postContent) {
+        setAlert(true);
+        const response = await fetch(postContent);
+        const blob = await response.blob();
+        const storageRef = ref(
+          storage,
+          `Post/${user._id}/${Math.round(Math.random() * 10000000)}`
+        );
+        const uploadTask = uploadBytesResumable(storageRef, blob);
+        uploadTask.on(
+          "state_changed",
+          (snapshot) => {
+            const progress = Math.round(
+              (snapshot.bytesTransferred / snapshot.totalBytes) * 100
+            );
+            setProgress(progress);
+          },
+          (error) => {
+            console.log(error)
+            setMessage(error);
+            setLoading(false);
+          },
+          () => {
+            getDownloadURL(uploadTask.snapshot.ref).then((downloadURL) => {
+              dataSender(downloadURL);
+            });
+          }
+        );
+      } else {
+        dataSender();
+      }
+    } catch (error) {
+      console.log(error)
+      setMessage(error);
+      setLoading(false);
+    }
+  };
+
+  const dataSender = async (postUrl) => {
+    try {
+      setLoading(true);
+      const token = await AsyncStorage.getItem("token");
+      const userData = await AsyncStorage.getItem("user-insta");
+
+      if (token && userData) {
+        let value = await JSON.parse(token);
+        const config = {
+          headers: {
+            "Content-Type": "application/json",
+            Accept: "application/json",
+            Authorization: `Bearer ${value}`,
+          },
+          withCredentials: true,
+          sameSite: "None",
+        };
+
+        const { data } = await axios.post(
+          `https://instagrambackend-one.vercel.app/api/v1/users/createpost`,
+          { desc, postUrl, postType: contentType },
+          config
+        );
+
+        if (data) {
+          console.log(data);
+          setLoading(false);
+        }
+      } else {
+        router.push("/login");
+      }
+    } catch (error) {
+      console.log(error);
+      setLoading(false);
+    }
+  };
+
+  const hideAlertHandler = () => {
+    setAlert(false);
+  };
+
   return (
     <ScrollView style={{ paddingTop: 25 }}>
       <View style={styles.header}>
@@ -69,6 +162,12 @@ const addPosts = () => {
           height: windowHeight / 1.5,
         }}
       >
+        <Alert
+          message={`${progress.toString()}%`}
+          hideAlertHandler={hideAlertHandler}
+          alert={alert}
+          tittle="Uploading..."
+        />
         {postContent ? (
           contentType == "video" ? (
             <Video
@@ -93,15 +192,20 @@ const addPosts = () => {
           </View>
         )}
       </View>
-      <View style={{padding: 10, marginTop: 10}}>
-        <TextInput style={styles.txtInp} placeholder="Description"></TextInput>
+      <View style={{ padding: 10, marginTop: 10 }}>
+        <TextInput
+          style={styles.txtInp}
+          placeholder="Description"
+          value={desc}
+          onChangeText={(value) => setDesc(value)}
+        ></TextInput>
       </View>
       <View
         style={{
           flexDirection: "row",
           justifyContent: "space-between",
           marginTop: 20,
-          paddingHorizontal: 10
+          paddingHorizontal: 10,
         }}
       >
         <TouchableOpacity
@@ -176,9 +280,9 @@ const styles = StyleSheet.create({
   },
   txtInp: {
     borderWidth: 1,
-    borderColor: 'gray',
+    borderColor: "gray",
     padding: 10,
     width: "100%",
-    borderRadius: 10
-  }
+    borderRadius: 10,
+  },
 });
